0522205211974f856e18ed0432e3e51f71bbf79f
joel ramsey <joel@joels-MacBook-Pro.local>
2016-10-15 16:42:09 -0600

    Added initial condition checks for match situations.

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,14 @@ var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
+
+//initial conditional checks
+
+//queens match followed by kings match folled by no match message
+if(cardOne === cardTwo){
+	alert("You found a match!");
+} else if(cardThree === cardFour){
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
