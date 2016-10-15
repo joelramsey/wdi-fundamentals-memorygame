2ac657af69b10fe9c7b94e410e53dcad53007681
joel ramsey <joel@joels-MacBook-Pro.local>
2016-10-15 16:04:46 -0600

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,5 @@
-console.log("JS file is connected to HTML! Woo!")
+//declare four variables representing the four cards (two sets of matches)
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
