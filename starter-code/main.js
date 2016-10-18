be16a7a550cff50980976cf276a682f2343f6d0b
joel ramsey <joel@joels-MacBook-Pro.local>
2016-10-17 18:29:32 -0600

    Replaced static html with js created cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -7,10 +7,30 @@ var cardFour = 'king';
 //initial conditional checks
 
 //queens match followed by kings match folled by no match message
-if(cardOne === cardTwo){
-	alert("You found a match!");
-} else if(cardThree === cardFour){
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+// if(cardOne === cardTwo){
+// 	alert("You found a match!");
+// } else if(cardThree === cardFour){
+// 	alert("You found a match!");
+// } else {
+// 	alert("Sorry, try again.");
+// }
+
+
+
+function createCards(){
+	//get the game board to append the cards
+	board = document.getElementById('game-board');
+	console.log(board);
+
+	//append the cards to the gameboard
+	for(i = 0; i < 4; i++){
+		var card = document.createElement('div');
+		card.className = 'card';
+		board.appendChild(card);
+	}
+	//just verifying creation
+	card_check = document.querySelectorAll('.card');
+	console.log(card_check);
 }
+
+createCards();
