9b2bc55e71232459be0a2ba48fa1c67d87cb75b5
joelramsey <joel.d.ramsey@gmail.com>
2016-10-17 22:39:58 -0600

    added js logic to check for matches and show the images of the selected cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,36 +1,82 @@
-//declare four variables representing the four cards (two sets of matches)
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+//declare array holding all four cards
+var cards = ['queen', 'queen', 'king', 'king'];
 
-//initial conditional checks
+var cardsInPlay = [];
 
-//queens match followed by kings match folled by no match message
-// if(cardOne === cardTwo){
-// 	alert("You found a match!");
-// } else if(cardThree === cardFour){
-// 	alert("You found a match!");
-// } else {
-// 	alert("Sorry, try again.");
-// }
-
-
-
-function createCards(){
+function createBoard(){
 	//get the game board to append the cards
 	board = document.getElementById('game-board');
 	console.log(board);
 
-	//append the cards to the gameboard
-	for(i = 0; i < 4; i++){
-		var card = document.createElement('div');
-		card.className = 'card';
-		board.appendChild(card);
+	//loop through the length of cards for the set up
+	for(i = 0; i < cards.length; i++){
+
+		//create the element as a div
+		var cardElement = document.createElement('div');
+
+		//provide a class name to the created element
+		cardElement.className = 'card';
+
+		//provide it some metadata so we know which one it is
+		cardElement.setAttribute('data-card', cards[i]);
+
+		//add event listener to execute isTwoCards on click
+		cardElement.addEventListener('click', isTwoCards);
+
+		//slap the card onto the board
+		board.appendChild(cardElement);
 	}
+	
 	//just verifying creation
 	card_check = document.querySelectorAll('.card');
 	console.log(card_check);
 }
 
-createCards();
+function isTwoCards(){
+	//push the king or queen value into the array
+	cardsInPlay.push(this.getAttribute('data-card'));
+
+	//check to see which it is and show the png of corresponding item
+	if(this.getAttribute('data-card') === 'queen'){
+		
+		this.innerHTML = "<img src='queen.png'>";
+	
+	} else {
+		
+		this.innerHTML = "<img src='king.png'>";
+	}
+
+	if(cardsInPlay.length === 2){
+		
+		//see if they are the same string value
+		isMatch(cardsInPlay);
+
+		//reset the array for the nxt round 
+		cardsInPlay = [];
+	}
+}
+
+function isMatch(){
+	
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		
+		alert("You found a match!");
+		//give me a second to view the second card before you wipe it
+		setTimeout(clear, 1000);
+
+	} else {
+		
+		alert("Sorry, try again.");
+		setTimeout(clear, 1000);
+	}
+}
+
+//added this function to clear the inner HTML of the cards, thus removing the images from view
+function clear(){
+	zero = document.getElementsByClassName('card');
+	for (i = 0; i < cards.length; i++){
+		zero[i].innerHTML = '';
+	}
+}
+
+createBoard();
